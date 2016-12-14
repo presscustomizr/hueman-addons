f06d910a7c29535a16c885280be17aa1791c5c17
nikeo <nicolas@presscustomizr.com>
2016-12-14 09:13:05 +0100

    .gitignore refreshed

--- grunt-tasks-config/clean.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  options : {
-    force: true //This overrides this task from blocking deletion of folders outside current working dir (CWD). Use with caution.
-  },
-	main : ['build/**/*']
-};
--- grunt-tasks-config/jshint.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	options : {
-		reporter : require('jshint-stylish'),
-      scripturl:true
-	},
-	gruntfile : ['Gruntfile.js'],
-	front_js : ['<%= paths.front_js %>*.js', '!<%= paths.front_js %>*.min.js'],
-	those : [], //populated dynamically with the watch event
-};
