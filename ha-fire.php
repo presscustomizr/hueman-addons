f785f7288b2b5c08336a2c6d992d30db0c1912e1
nikeo <nicolas@presscustomizr.com>
2016-12-15 16:17:31 +0100

    v2.0-beta4 build

--- ha-fire.php
+++ ha-fire.php
@@ -3,7 +3,7 @@
 * Plugin Name: Hueman Addons
 * Plugin URI: http://presscustomizr.com
 * Description: Hueman Theme Addons
-* Version: 2.0-beta-3
+* Version: 2.0-beta4
 * Text Domain: hueman-addons
 * Author: Press Customizr
 * Author URI: http://presscustomizr.com
