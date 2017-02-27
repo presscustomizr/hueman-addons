ec263663643195069bf4329e1192b037cfa135c1
nikeo <nicolas@presscustomizr.com>
2017-02-27 17:29:31 +0100

    fixed : added hu_is_real_home as a pluggable function in both Hueman theme and Hueman addon

--- ha-fire.php
+++ ha-fire.php
@@ -10,4 +10,17 @@
 * License: GPLv2 or later
 */
 
+/**
+* helper ( can be already defined in the hueman theme)
+* Check if we are really on home, all cases covered
+* @return  bool
+*/
+if ( ! function_exists( 'hu_is_real_home') ) {
+  function hu_is_real_home() {
+    return ( is_home() && ( 'posts' == get_option( 'show_on_front' ) || '__nothing__' == get_option( 'show_on_front' ) ) )
+    || ( 0 == get_option( 'page_on_front' ) && 'page' == get_option( 'show_on_front' ) )//<= this is the case when the user want to display a page on home but did not pick a page yet
+    || is_front_page();
+  }
+}
+
 require_once( plugin_dir_path( __FILE__ ) . 'addons/ha-init.php' );
