f6cdbf5d664d966392090ea56133e4ec1337ec04
nikeo <nicolas@presscustomizr.com>
2018-06-26 12:12:05 +0200

    added new utilities

--- addons/ha-functions.php
+++ addons/ha-functions.php
@@ -81,3 +81,53 @@ function ha_is_plugin_active_for_network( $plugin ) {
 
   return false;
 }
+
+
+//@return bool
+function ha_isprevdem() {
+    global $wp_customize;
+    $is_dirty = false;
+
+    if ( is_object( $wp_customize ) && method_exists( $wp_customize, 'unsanitized_post_values' ) ) {
+        $real_cust            = $wp_customize -> unsanitized_post_values( array( 'exclude_changeset' => true ) );
+        $_preview_index       = array_key_exists( 'customize_messenger_channel' , $_POST ) ? $_POST['customize_messenger_channel'] : '';
+        $_is_first_preview    = false !== strpos( $_preview_index ,'-0' );
+        $_doing_ajax_partial  = array_key_exists( 'wp_customize_render_partials', $_POST );
+        //There might be cases when the unsanitized post values contains old widgets infos on initial preview load, giving a wrong dirtyness information
+        $is_dirty             = ( ! empty( $real_cust ) && ! $_is_first_preview ) || $_doing_ajax_partial;
+    }
+    // was formerly using is_child_theme() but it has been dropped because of https://github.com/presscustomizr/hueman-addons/issues/46#issuecomment-366235847
+    // => this function can't be invoked too early from a plugin because TEMPLATEPATH and STYLESHEETPATH may not be defined yet
+    return apply_filters( 'hu_isprevdem', ! $is_dirty && ha_get_raw_option( 'template', null, false ) != get_template() && ! ( defined('HU_IS_PRO') && HU_IS_PRO ) );
+}
+
+
+
+// @return bool
+// skop shall not be activated when previewing the theme from the customizer or when using a version of wp < 4.7
+function ha_is_skop_on() {
+    global $wp_version;
+    if( ! defined( 'HA_SKOP_ON' ) ) {
+        //'enable-skope' option can take two string values : "yes" and "no".
+        //If the option is not set yet, which is the most common case, it means that it is enabled ( @see default value == "yes" when registering the setting )
+        $_skope_enable_val = ha_get_raw_option( 'enable-skope' , 'hu_theme_options');
+        define( 'HA_SKOP_ON' , ! is_string( $_skope_enable_val ) || 'yes' == $_skope_enable_val );
+    }
+    return apply_filters( 'ha_is_skop_on', HA_SKOP_ON && ! ha_isprevdem() && version_compare( $wp_version, '4.7', '>=' ) );
+}
+
+
+
+
+
+
+
+function sek_error_log( $title, $content = null ) {
+    if ( is_null( $content ) ) {
+        error_log( '<' . $title . '>' );
+    } else {
+        error_log( '<' . $title . '>' );
+        error_log( print_r( $content, true ) );
+        error_log( '</' . $title . '>' );
+    }
+}
